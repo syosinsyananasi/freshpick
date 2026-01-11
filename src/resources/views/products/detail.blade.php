d553aab87188270f972b9e39e52421f293efc8ca
nanashi <cibicco323@gmail.com>
2026-01-11 17:06:54 +0900

    design-ui実装

--- src/resources/views/products/detail.blade.php
+++ src/resources/views/products/detail.blade.php
@@ -75,10 +75,16 @@
         </div>
 
         <div class="product-detail__actions">
-            <a href="/products" class="product-detail__back-btn">戻る</a>
-            <button type="submit" class="product-detail__submit-btn">変更を保存</button>
+            <div class="product-detail__actions-center">
+                <a href="/products" class="product-detail__back-btn">戻る</a>
+                <button type="submit" class="product-detail__submit-btn">変更を保存</button>
+            </div>
             <a href="/products/{{ $product->id }}/delete" class="product-detail__delete-btn" onclick="return confirm('本当に削除しますか？');">
-                <span class="product-detail__delete-icon">&#128465;</span>
+                <svg class="product-detail__delete-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19 6H16V5C16 3.897 15.103 3 14 3H10C8.897 3 8 3.897 8 5V6H5C4.447 6 4 6.447 4 7C4 7.553 4.447 8 5 8H6V19C6 20.103 6.897 21 8 21H16C17.103 21 18 20.103 18 19V8H19C19.553 8 20 7.553 20 7C20 6.447 19.553 6 19 6ZM10 5H14V6H10V5ZM16 19H8V8H16V19Z" fill="currentColor"/>
+                    <path d="M10 10H11V17H10V10Z" fill="currentColor"/>
+                    <path d="M13 10H14V17H13V10Z" fill="currentColor"/>
+                </svg>
             </a>
         </div>
     </form>
